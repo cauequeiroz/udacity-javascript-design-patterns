70a27b9aed026211adff99770dfdf54be400e605
Caue Queiroz <cauenqueiroz@gmail.com>
2016-09-06 13:09:38 -0300

    Change code to a MVO structure

--- app.js
+++ app.js
@@ -1,74 +1,111 @@
-var cats = [
-	{
-		name: 'Pixel',
-		image: 'img/cat-1.jpg',
-		count: 0
-	},
-	{
-		name: 'Tom',
-		image: 'img/cat-2.jpg',
-		count: 0
-	},
-	{
-		name: 'Hanna',
-		image: 'img/cat-3.jpg',
-		count: 0
-	},
-	{
-		name: 'Guerry',
-		image: 'img/cat-4.jpg',
-		count: 0
-	},{
-		name: 'Oz',
-		image: 'img/cat-5.jpg',
-		count: 0
-	},
-	{
-		name: 'Fold',
-		image: 'img/cat-6.jpg',
-		count: 0
-	}
-];
+var model = {
+	cats: [
+		{
+			name: 'Pixel',
+			image: 'img/cat-1.jpg',
+			count: 0
+		},
+		{
+			name: 'Tom',
+			image: 'img/cat-2.jpg',
+			count: 0
+		},
+		{
+			name: 'Hanna',
+			image: 'img/cat-3.jpg',
+			count: 0
+		},
+		{
+			name: 'Guerry',
+			image: 'img/cat-4.jpg',
+			count: 0
+		},{
+			name: 'Oz',
+			image: 'img/cat-5.jpg',
+			count: 0
+		},
+		{
+			name: 'Fold',
+			image: 'img/cat-6.jpg',
+			count: 0
+		}
+	],
 
-var generateList = function() {
-	var $ul  = document.createElement('ul');		
+	getAllCats: function() {
+		return this.cats;
+	},
+
+	addCount: function(cat) {
+		this.cats[cat].count++;
+	}
+}
+
+var octopus = {
+	init: function() {
+		view.renderList();
+		document.querySelectorAll('.cat-list li')[0].click();
+	},
+
+	getCats: function() {
+		return model.getAllCats();
+	},
+
+	selectCat: function() {
+		if ( this.classList.contains('selected') ) return;
+
+		var catSelected = document.querySelector('.cat-list .selected'),
+			cat = this.getAttribute('data-ref');
+
+		if ( catSelected ) {
+			catSelected.classList.remove('selected');
+		}
+		this.classList.add('selected');
+
+		view.renderCat(cat);
+	},
+
+	addCount: function() {
+		var cat = this.getAttribute('data-ref');
+		model.addCount(cat);
+		view.renderCat(cat);
+	}
+}
+
+
+var view = {
+	renderList: function() {
+		var $ul  = document.createElement('ul'),
+			cats = octopus.getCats();		
 	
-	for ( var i in cats ) {
-		var $li = document.createElement('li');
-			$li.innerHTML = cats[i].name;
-			$li.setAttribute('data-ref', i);
+		for ( var i in cats ) {
+			var $li = document.createElement('li');
+				$li.innerHTML = cats[i].name;
+				$li.setAttribute('data-ref', i);
 
-		$li.addEventListener('click', selectCat, false);
-		$ul.appendChild($li);
+			$li.addEventListener('click', octopus.selectCat, false);
+			$ul.appendChild($li);
+		}
+
+		document.querySelector('.cat-list').appendChild($ul);
+	},
+
+	renderCat: function(cat) {
+		var html = '',
+			cats = octopus.getCats(),
+			$ctn = document.querySelector('.cat-view');
+
+		html = '<p>'+cats[cat].name+': <span>'+cats[cat].count+'</span></p><img src="'+cats[cat].image+'" data-ref="'+cat+'">';
+		$ctn.innerHTML = html;
+
+		$ctn.querySelector('img').addEventListener('click', octopus.addCount, false);
 	}
+}
 
-	document.querySelector('.cat-list').appendChild($ul);
-};
+// Initialize app
+octopus.init();
 
-var selectCat = function() {
-	if ( this.classList.contains('selected') ) return;
 
-	if ( document.querySelector('.cat-list .selected') ) {
-		document.querySelector('.cat-list .selected').classList.remove('selected');
-	}
-	this.classList.add('selected');
-	console.log('change..');
 
-	var html = '',
-		ref  = this.getAttribute('data-ref'),
-		$ctn = document.querySelector('.cat-view');
 
-	html = '<p>'+cats[ref].name+': <span>'+cats[ref].count+'</span></p><img src="'+cats[ref].image+'" data-ref="'+ref+'">';
-	$ctn.innerHTML = html;
-
-	$ctn.querySelector('img').addEventListener('click', addCount, false);
-};
-
-var addCount = function() {
-	var $count = this.parentNode.querySelector('p span');
-	$count.innerHTML = ++cats[this.getAttribute('data-ref')].count;
-};
-
-generateList();
 
 
